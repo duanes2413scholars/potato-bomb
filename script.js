b06ca9c8cd7b69dabf8086b7cb919b826e9c9f66
duanes2413scholars <duanes@scholarsnyc.com>
2013-06-04 14:04:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
+	
 	var canvas = $("#canvas")[0];
 	var ctx = canvas.getContext("2d");
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
+	
 	
 	var cw = 10;
 	var d;
 	var food;
 	var score;
 	
+	
 	var snake_array; 
 	
 	function init()
 	{
-		d = "right";
+		d = "right"; 
 		create_snake();
-		create_food();
+		create_food(); 
+	
 		score = 0;
+		
 		
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
 		game_loop = setInterval(paint, 60);
@@ -29,10 +34,11 @@ $(document).ready(function(){
 		snake_array = []; 
 		for(var i = length-1; i>=0; i--)
 		{
-			
+		
 			snake_array.push({x: i, y:0});
 		}
 	}
+	
 	
 	function create_food()
 	{
@@ -43,32 +49,34 @@ $(document).ready(function(){
 	
 	}
 	
-
+	
 	function paint()
 	{
-	
+		
 		ctx.fillStyle = "white";
 		ctx.fillRect(0, 0, w, h);
 		ctx.strokeStyle = "black";
 		ctx.strokeRect(0, 0, w, h);
 		
+	
 		var nx = snake_array[0].x;
 		var ny = snake_array[0].y;
-		
+	
 		if(d == "right") nx++;
 		else if(d == "left") nx--;
 		else if(d == "up") ny--;
 		else if(d == "down") ny++;
 		
+	
 		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
 		{
-		
-			init();
 			
+			init();
+		
 			return;
 		}
 		
-	
+		
 		if(nx == food.x && ny == food.y)
 		{
 			var tail = {x: nx, y: ny};
@@ -83,11 +91,12 @@ $(document).ready(function(){
 		}
 	
 		
-		snake_array.unshift(tail); 
+		snake_array.unshift(tail);  
+		
 		for(var i = 0; i < snake_array.length; i++)
 		{
 			var c = snake_array[i];
-			
+		
 			paint_cell(c.x, c.y);
 		}
 		
@@ -98,7 +107,7 @@ $(document).ready(function(){
 		ctx.fillText(score_text, 5, h-5);
 	}
 	
-
+	
 	function paint_cell(x, y)
 	{
 		ctx.fillStyle = "blue";
@@ -127,5 +136,12 @@ $(document).ready(function(){
 		else if(key == "39" && d != "left") d = "right";
 		else if(key == "40" && d != "up") d = "down";
 	
-	})	
+	})
+	
+	
+	
+	
+	
+	
+	
 })
